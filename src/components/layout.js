70b6220f80aa0f821edc481f3fbff9505825be0b
Livingstone <livingstone23@gmail.com>
2020-03-21 06:19:13 -0600

    Estructrua basica
    Se definieron primeros componentes y estructura basica

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,50 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/core';
+import Header from './header';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+const layout = (props) => {
+    return (
+        <>
+            <Global 
+                styles={css`
+                    html {
+                        font-size: 62.5%;
+                    }
+                    body {
+                        font-size: 18px;
+                        font-size: 1.8rem;
+                        line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    h1 , h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                `}
+            />
+            <Helmet>
+                <title>Restaurante La Cabañita</title>
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
+            </Helmet>
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+            <Header />
+            {props.children}
+        </>
+    );
+};
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default layout;
